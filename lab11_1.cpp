8a99a5f7436e32f677590dab78a9593fd6578463
fongtakamon <noofongka@gmail.com>
2023-12-22 17:20:38 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,36 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void printResult() {
+    string thegrade ;
+    srand(time(0));
+    int x = rand()%9;
+    if(x==0) thegrade = "A" ;
+    if(x==1) thegrade = "B+" ;
+    if(x==2) thegrade = "B" ;
+    if(x==3) thegrade = "C+" ;
+    if(x==4) thegrade = "C" ;
+    if(x==5) thegrade = "D+" ;
+    if(x==6) thegrade = "D" ;
+    if(x==7) thegrade = "F" ;
+    if(x==8) thegrade = "W" ;
+cout << "You will get " << thegrade <<" in this 261102." <<  endl ;
+
+}
+
+int main(){
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    int grade = rand() % 9 ;
+    
+printResult();
+
+return 0;
+ 
+}
